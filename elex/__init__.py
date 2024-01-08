85b67a974c62e7b7b6e561640ecdc2b25a42c539
Tom Nehil <tom.nehil@gmail.com>
2024-01-08 16:20:29 -0600

    Updates request url to https per AP security update, and changes default api version to v3

--- elex/__init__.py
+++ elex/__init__.py
@@ -10,8 +10,8 @@ __version__ = '2.4.3'
 _DEFAULT_CACHE_DIRECTORY = os.path.join(tempfile.gettempdir(), 'elex-cache')
 
 API_KEY = os.environ.get('AP_API_KEY', None)
-API_VERSION = os.environ.get('AP_API_VERSION', 'v2')
-BASE_URL = os.environ.get('AP_API_BASE_URL', 'http://api.ap.org/{0}'.format(API_VERSION))
+API_VERSION = os.environ.get('AP_API_VERSION', 'v3')
+BASE_URL = os.environ.get('AP_API_BASE_URL', 'https://api.ap.org/{0}'.format(API_VERSION))
 CACHE_DIRECTORY = os.environ.get('ELEX_CACHE_DIRECTORY', _DEFAULT_CACHE_DIRECTORY)
 
 session = requests.session()
